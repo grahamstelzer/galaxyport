f32711b1d30b9ed50aac60bef885f47c67bad8a7
Graham Stelzer <graham.stelzer.01@gmail.com>
2025-05-11 09:38:31 -0400

    fixed comment in titleNode that causes build crash

--- everything/src/Workflow/TitleNode.tsx
+++ everything/src/Workflow/TitleNode.tsx
@@ -23,7 +23,7 @@ export default function CustomNode({id, data:{text, title}}:NodeProps<{text:stri
             alignItems={"center"}
             borderRadius="5px"
             bg={"black"}
-            border="0px solid" {/* EDIT THIS AND ALSO IN WORKFLOW FOR BORDERS*/}
+            border="0px solid" 
             borderColor={"white"}
             p={2}
             gap={2}
